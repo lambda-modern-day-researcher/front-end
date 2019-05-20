5768ea814eb7a66092d3a15f31afba0479f8f1eb
Andy Bettisworth <andy@accreu.com>
2019-05-20 18:26:59 -0500

    Test the deployment

--- client/src/components/SignInUp/SignInUp.js
+++ client/src/components/SignInUp/SignInUp.js
@@ -32,36 +32,40 @@ class SignInUp extends Component {
 
   render() {
     return (
-      <styles.SignInUpStyle>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-10 col-md-8 col-lg-5">
-
-              <p className="text-center mt-4">LET'S DO THIS</p>
-
-              <h2 className="text-center mb-4">Time to manage link chaos</h2>
-
-              <form className="mb-10">
-                <div className="form-group">
-                  <label for="input_email" className="text-primary">Email <span className="text-danger">*</span></label>
-                  <input id="input_email" type="email" name="email" className="form-control" autofocus="true" value={this.state.email} onChange={this.handleOnChange} required="true"></input>
-                </div>
-
-                <div className="form-group">
-                  <label for="input_password" className="text-primary">Password <span className="text-danger">*</span></label>
-                  <input id="input_password" type="password" name="password" className="form-control" value={this.state.password} onChange={this.handleOnChange} required="true"></input>
-                </div>
-
-                <button type="submit" className="btn btn-block btn-primary">Get started</button>
-              </form>
-
-            </div>
-          </div>
-        </div>
-      </styles.SignInUpStyle>
+      <>
+        <h1>SignInUp View</h1>
+      </>
     )
   }
 }
+
+// <styles.SignInUpStyle>
+//   <div className="container">
+//     <div className="row justify-content-center">
+//       <div className="col-10 col-md-8 col-lg-5">
+//
+//         <p className="text-center mt-4">LET'S DO THIS</p>
+//
+//         <h2 className="text-center mb-4">Time to manage link chaos</h2>
+//
+//         <form className="mb-10">
+//           <div className="form-group">
+//             <label for="input_email" className="text-primary">Email <span className="text-danger">*</span></label>
+//             <input id="input_email" type="email" name="email" className="form-control" autofocus="true" value={this.state.email} onChange={this.handleOnChange} required="true"></input>
+//           </div>
+//
+//           <div className="form-group">
+//             <label for="input_password" className="text-primary">Password <span className="text-danger">*</span></label>
+//             <input id="input_password" type="password" name="password" className="form-control" value={this.state.password} onChange={this.handleOnChange} required="true"></input>
+//           </div>
+//
+//           <button type="submit" className="btn btn-block btn-primary">Get started</button>
+//         </form>
+//
+//       </div>
+//     </div>
+//   </div>
+// </styles.SignInUpStyle>
 
 /**
  * Export component
