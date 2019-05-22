625cae2ec6e9e4d33bb5a553e99de386c2ef019e
Andy Bettisworth <andy@accreu.com>
2019-05-22 12:20:25 -0500

    Revert mistake deletions

--- client/src/routes/RootRouter.js
+++ client/src/routes/RootRouter.js
@@ -1,9 +1,40 @@
 'use strict'
 
 /**
- * Export route helpers
+ * Dependencies
  */
 
-module.exports = {
-  PrivateRoute: require('./PrivateRoute'),
+const React = require('react')
+const react_router_dom = require('react-router-dom')
+const views = require('../views/index')
+
+/**
+ * Constants
+ */
+
+const Route = react_router_dom.Route
+const Redirect = react_router_dom.Redirect
+
+/**
+ * Define router
+ */
+
+function RootRouter() {
+  return (
+    <>
+      <Route exact path="/" render={() => <Redirect to="/users/signin" />} />
+      <Route path="/home" component={views.Home} />
+      <Route path="/friends" component={views.Friends} />
+      <Route path="/login" render={() => <Redirect to="/users/signin" />} />
+      <Route path="/signin" render={() => <Redirect to="/users/signin" />} />
+      <Route path="/signup" render={() => <Redirect to="/users/signup" />} />
+      <Route path="/register" render={() => <Redirect to="/users/signup" />} />
+    </>
+  )
 }
+
+/**
+ * Export router
+ */
+
+module.exports = RootRouter
