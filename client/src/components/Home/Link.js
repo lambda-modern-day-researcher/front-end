1c49a3c2d64a6e0be511c561b6ed4349dd78e1af
Andy Bettisworth <andy@accreu.com>
2019-05-23 15:25:31 -0500

    console.log

--- client/src/components/Home/Link.js
+++ client/src/components/Home/Link.js
@@ -19,6 +19,8 @@ const Component = React.Component
 const connect = react_redux.connect
 const toggleLinkPriority = actions.toggleLinkPriority
 const completeLink = actions.completeLink
+const getPriorityLinks = actions.getPriorityLinks
+const getMainLinks = actions.getMainLinks
 
 /**
  * Define component
@@ -42,10 +44,20 @@ class Link extends Component {
 
   handleCompleteLink = (event) => {
     this.props.completeLink(this.state.user_id, this.state.link_id)
+      .then(() => {
+        document.activeElement.blur()
+        this.props.getPriorityLinks(this.props.usersReducer.current_user_id)
+        this.props.getMainLinks(this.props.usersReducer.current_user_id)
+      })
   }
 
   handleToggleLinkPriority = (event) => {
     this.props.toggleLinkPriority(this.state.user_id, this.state.link_id)
+      .then(() => {
+        document.activeElement.blur()
+        this.props.getPriorityLinks(this.props.usersReducer.current_user_id)
+        this.props.getMainLinks(this.props.usersReducer.current_user_id)
+      })
   }
 
   // handleClose = value => {
@@ -59,7 +71,7 @@ class Link extends Component {
           <div className="col-12 col-lg-2 col-xl-1 d-flex">
             <svg onClick={this.handleCompleteLink} className="control control__completed" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 
-            <svg onClick={this.handleToggleLinkPriority} className="control control__priority" style={(this.props.isPriority) ? { fill: 'red' } : { fill: '#AAA' }}
+            <svg onClick={this.handleToggleLinkPriority} className="control control__priority" style={(this.props.is_pinned === 1) ? { fill: 'red' } : { fill: '#AAA' }}
                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 
             <svg className="control control__share" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
@@ -100,4 +112,9 @@ const mapStateToProps = (state) => {
  * Export component
  */
 
-module.exports = connect(mapStateToProps, { toggleLinkPriority, completeLink })(Link)
+module.exports = connect(mapStateToProps, {
+  toggleLinkPriority,
+  completeLink,
+  getPriorityLinks,
+  getMainLinks
+})(Link)
