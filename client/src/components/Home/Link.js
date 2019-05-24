ab386879afe207f27e2251ed3741f4e79427c3a4
Andy Bettisworth <andy@accreu.com>
2019-05-24 11:56:35 -0500

    Firebase deploy

--- client/src/components/Home/Link.js
+++ client/src/components/Home/Link.js
@@ -136,7 +136,7 @@ class Link extends Component {
             <svg onClick={this.handleShareLinkWithOthers} className="control control__share" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
           </div>
 
-          <div className="col-6 col-lg-9 col-xl-10">
+          <div className="col-10 col-lg-9 col-xl-10">
             <a href={this.props.url} target="_blank" className="a__link">
               <div>
                 <p className="text__title">
@@ -152,7 +152,7 @@ class Link extends Component {
             </ul>
           </div>
 
-          <div className="col-6 col-lg-1 col-xl-1">
+          <div className="col-2 col-lg-1 col-xl-1">
             <span className="btn__control" onClick={this.handleEditLinkTitle}>edit</span>
             <span className="btn__control" onClick={this.handleDeleteLink}>x</span>
           </div>
